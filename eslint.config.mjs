6493b7d5b053c17eada8ce34642f3c2b85434a1c
tylertran <conian2505@gmail.com>
2024-04-27 10:11:30 +0700

    feat: setup eslint v2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
-import js from "@eslint/js";
-export default [
-  js.configs.recommended,
-
+module.exports = [
   {
     //* "off" or 0 - turn the rule off
     //* "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
     //* "error" or 2 - turn the rule on as an error (exit code will be 1)
+    parser: "@typescript-eslint/parser",
+    languageOptions: {
+      parser: "@typescript-eslint/parser"
+    },
+    extends: ["plugin:@typescript-eslint/recommended"],
+    parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    // 0 = off, 1 = warn, 2 = error
     rules: {
       "@typescript-eslint/explicit-member-accessibility": 0,
       "@typescript-eslint/explicit-function-return-type": 0,
@@ -15,14 +22,7 @@ export default [
       "@typescript-eslint/no-explicit-any": 0,
       "@typescript-eslint/ban-types": 0,
       "@typescript-eslint/no-var-requires": 0,
-      "@typescript-eslint/no-empty-function": 0,
-      "--no-warn-ignored": 0
-    },
-    parser: "@typescript-eslint/parser",
-    extends: ["prettier", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
+      "@typescript-eslint/no-empty-function": 1,
     },
   },
 ];
