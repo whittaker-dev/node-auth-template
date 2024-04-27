4c32e41b37e8273aaa36e0bcd286fa5ed896a17b
tylertran <conian2505@gmail.com>
2024-04-27 10:53:21 +0700

    refactor: test eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-const tsEslint = require("@typescript-eslint/parser");
 module.exports = [
   {
     rules: {
       semi: "error",
       "prefer-const": "error",
+      "no-unused-vars": "error",
     },
   },
 ];
