38e1bd49f690314490331674c34fc7132e03de7a
conianguys <conian2505@gmail.com>
2024-06-17 20:30:09 +0700

    feat: update docker compose and logger

--- src/api/logger/index.ts
+++ src/api/logger/index.ts
@@ -9,18 +9,18 @@ const logger = winston.createLogger({
       return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
     }),
   ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      level: "info",
-      format: winston.format.printf((info) => {
-        return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
-      }),
-      filename: "/src/api/logger",
-      maxsize: 1,
-    }),
-    new winston.transports.Http({ host: "localhost", port: 8080 }),
-  ],
+  // transports: [
+  //   new winston.transports.Console(),
+  //   new winston.transports.File({
+  //     level: "info",
+  //     format: winston.format.printf((info) => {
+  //       return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+  //     }),
+  //     filename: "logger.ts",
+  //     maxsize: 1,
+  //   }),
+  //   new winston.transports.Http({ host: "localhost", port: 8080 }),
+  // ],
 });
 logger.info("winston transports");
 export default logger;
